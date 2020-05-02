f7c84f33abffd728fb2c7f257f4efaa3e17b55c6
RanaElshafie <rana.elshafie@vodafone.com>
2020-05-02 23:44:25 +0200

    completeing make cache matrix function and get the inverse and check it

--- cachematrix.R
+++ cachematrix.R
@@ -4,6 +4,19 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+  cacheM <- NULL
+  setM <- function(y) {
+    x <<- y
+    cacheM <<- NULL
+    getM <- function() x
+    setCache <- function(inverse) cacheM <<- inverse
+    getCache <- function() cacheM
+    list(setM = setM,
+         getM = getM,
+         setCache = setCache,
+         getCache = getCache)
+    
+  }
 
 }
 
@@ -12,4 +25,15 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  cacheM <- x$getCache()
+  if (!is.null(cacheM)) {
+    message("load cache matrix")
+    return(cacheM)
+  }
+  else {
+    dMatrix <- x$getM()
+    cacheMatrix <- solve(dMatrix, ...)
+    x$setCache(cacheM)
+    return(cacheM)
+  }
 }
