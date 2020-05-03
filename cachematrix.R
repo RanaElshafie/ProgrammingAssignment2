9567faabf9be16b6f79e5f17d04ba9036626deff
RanaElshafie <rana.elshafie@vodafone.com>
2020-05-03 22:02:03 +0200

    adding comments to makeCacheMatrix and cachesolve functions

--- cachematrix.R
+++ cachematrix.R
@@ -2,15 +2,25 @@
 ## functions do
 
 ## Write a short comment describing this function
-
+# create makeCacheMatrix function with argument x 
 makeCacheMatrix <- function(x = matrix()) {
+
+  # create cache matrix from passed matrix
+  # 1. initialize the cache Matrix (cacheM) and assign Null value for it
+ 
   cacheM <- NULL
+ #  function to set Matrix (setM)
   setM <- function(y) {
     x <<- y
     cacheM <<- NULL
+  }
+  # create function getM to return function x
     getM <- function() x
+    # create function  to set the cached inverse of 'x'
     setCache <- function(inverse) cacheM <<- inverse
+    # create function  to return the cached inverse of 'x'
     getCache <- function() cacheM
+    # 6. list the names of all function that will be known to the outside world
     list(setM = setM,
          getM = getM,
          setCache = setCache,
@@ -18,18 +28,22 @@ makeCacheMatrix <- function(x = matrix()) {
     
   }
 
-}
 
 
-## Write a short comment describing this function
+
+# 'cacheSolve return the inverse of a given matrix utilizing the cache
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  #  check  cache matrix
   cacheM <- x$getCache()
+  #  if the cacheM is not null then: return the result 
   if (!is.null(cacheM)) {
     message("load cache matrix")
     return(cacheM)
   }
+  #  if the cacheM is empty then: 
+  # get the matrix, create, set, update and return the cache matrix
+ 
   else {
     dMatrix <- x$getM()
     cacheMatrix <- solve(dMatrix, ...)
